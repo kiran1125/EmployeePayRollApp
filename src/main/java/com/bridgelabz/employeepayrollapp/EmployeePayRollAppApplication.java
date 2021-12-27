91fa37461d885f21f8ae4e97cecee14c2cfa7648
Kiran <makamkiran95@gmail.com>
2021-12-27 20:33:17 +0530

    Added : Usage of Slf4j for logging purpose

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
@@ -2,12 +2,17 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayRollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayRollAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayRollAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",context.getEnvironment().getProperty("environment"));
 	}
 
 }
