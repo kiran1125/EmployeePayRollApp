ebd80baa69a4f3f906919853fd2c8e189459357a
Kiran <makamkiran95@gmail.com>
2021-12-28 17:21:01 +0530

    Added : Validations and exception to the extra added properties

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayRollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayRollDTO.java
@@ -4,11 +4,15 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
+import lombok.NonNull;
 
 public @Data class EmployeePayRollDTO {
 
@@ -19,11 +23,21 @@ public @Data class EmployeePayRollDTO {
 	@Min(value = 500, message = "Min wage is not more than 500")
 	private long salary;
 	
+	@Pattern(regexp = "male|female", message = "Employee should be male or female")
 	private String gender;
+
 	@JsonFormat(pattern = "dd-MM-yyyy")
+	@NotNull (message = "startDate Should not be Empty")
+	@PastOrPresent(message = "Date should be present or past")
 	private LocalDate startDate;
+
+	@NotBlank(message = "note should not be blank")
 	private String note;
+
+	@NotBlank(message = "pic should not be blank")
 	private String profilePic;
+
+	@NotNull(message = "departments should not be blank")
 	private List<String> departments;
 
 }
--- src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayRollExceptionHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayRollExceptionHandler.java
@@ -7,12 +7,16 @@ import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class EmployeePayRollExceptionHandler extends RuntimeException {
     
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -26,6 +30,12 @@ public class EmployeePayRollExceptionHandler extends RuntimeException {
     @ExceptionHandler(EmployeePayRollException.class)
 	public ResponseEntity<ResponseDTO> handlerEmployeePayrollException(EmployeePayRollException exception) {
 		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request ", exception.getMessage());
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ResponseDTO> handlerHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+		log.error("Invalid Date Format", exception);
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request ", "Date format should be dd-MM-yyyy");
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
 	}
 }
